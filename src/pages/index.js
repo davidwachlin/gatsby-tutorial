cc7294b864d4b2d6ae6f9de055050ca032067ec6
David Wachlin <davidwachlin@gmail.com>
2019-12-07 02:44:06 -0500

    styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+import { Button } from '../components/button'
+
 export default () => (
   <Layout>
-    <h1 className="title">
-      this is our home page
-    </h1>
+    <h1 className="title">this is our home page</h1>
     <h1 className="title">another heading</h1>
     <h2>this is an h2 heading</h2>
     Hello world! Hello people!
+    <Button>styled button</Button>
   </Layout>
 )
