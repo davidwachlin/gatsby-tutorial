2d9b8acd4259697f0a8ab591d993c8c5b55b08f5
David Wachlin <davidwachlin@gmail.com>
2019-12-07 01:05:36 -0500

    css modules

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 import Layout from '../components/layout'
 
+import styles from '../components/blog.module.css'
+
 export default class blog extends Component {
   render() {
-    return <Layout><h1>this is our blog page</h1></Layout>
+    return <Layout>
+      <div className={styles.blog}>
+      <h1 className={styles.title}>this is our blog page</h1>
+      <h2>this is an h2 heading</h2>
+      </div>
+      </Layout>
+
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,11 @@ import Layout from '../components/layout'
 
 export default () => (
   <Layout>
-    <h1>
+    <h1 className="title">
       this is our home page
     </h1>
-    <h1>another heading</h1>
+    <h1 className="title">another heading</h1>
+    <h2>this is an h2 heading</h2>
     Hello world! Hello people!
   </Layout>
 )
