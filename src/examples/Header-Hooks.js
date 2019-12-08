91ca7c8c72da6954f2505cc681d9678050e304d0
David Wachlin <davidwachlin@gmail.com>
2019-12-07 20:41:19 -0500

    page query

--- src/examples/Header-Hooks.js
+++ src/examples/Header-Hooks.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
+const getData = graphql`
+  query {
+    site {
+      siteMetadata {
+        siteTitle: title
+        description
+        author
+        data {
+          age
+        }
+      }
+    }
+  }
+`
 
 const Header = () => {
   const {
     site: { siteMetadata: info },
-  } = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          description
-          title
-          author
-          data {
-            age
-          }
-        }
-      }
-    }
-  `)
-  console.log(info)
+  } = useStaticQuery(getData)
+  // console.log(info)
   return (
     <div>
       {/* <h1>author: {data.site.siteMetadata.author}</h1> */}
       <h1>author: {info.author}</h1>
-      <h1>title: {info.title}</h1>
+      <h1>title: {info.siteTitle}</h1>
     </div>
   )
 }
--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,14 +1,38 @@
 import React from 'react'
-import Header from '../examples/Header'
+import HeaderHook from '../examples/Header-Hooks'
+import HeaderStatic from '../examples/Header-Static'
+import { graphql } from 'gatsby'
 
-
-const examples = () => {
-    return (
-        <div>
-            examples page
-            <Header />
-        </div>
-    )
+const examples = props => {
+  const { name, age } = props.data.site.siteMetadata.person
+  return (
+    <div>
+      examples page
+      <HeaderHook />
+      <HeaderStatic />
+      <div>
+          this is our person
+          <h1>name: {name}</h1>
+          <h1>age: {age}</h1>
+      </div>
+    </div>
+  )
 }
+
+export const getData = graphql`
+  query {
+    site {
+      siteMetadata {
+        siteTitle: title
+        description
+        author
+        person: data {
+          age
+          name
+        }
+      }
+    }
+  }
+`
 
 export default examples
