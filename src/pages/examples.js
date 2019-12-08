7a4139ee960d1d2c2b4f3862a012b0d82ca69f32
David Wachlin <davidwachlin@gmail.com>
2019-12-07 21:56:33 -0500

    gatsby-source-filesystem

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -2,11 +2,12 @@ import React from 'react'
 import HeaderHook from '../examples/Header-Hooks'
 import HeaderStatic from '../examples/Header-Static'
 import { graphql } from 'gatsby'
+import Layout from '../components/layout'
 
 const examples = props => {
   const { name, age } = props.data.site.siteMetadata.person
   return (
-    <div>
+    <Layout>
       examples page
       <HeaderHook />
       <HeaderStatic />
@@ -15,7 +16,7 @@ const examples = props => {
           <h1>name: {name}</h1>
           <h1>age: {age}</h1>
       </div>
-    </div>
+    </Layout>
   )
 }
 
